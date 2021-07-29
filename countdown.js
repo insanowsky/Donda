dddc6955cf8ab09872f45a6c68cb408ff54906cf
Mikołaj Olszyński <insanowskyy@gmail.com>
2021-07-30 00:24:57 +0200

    twitter embed + gdrive link at bottom section of website

---
+++ countdown.js
@@ -0,0 +1,32 @@
+(function () {
+    const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+    let donda = "Aug 6, 2021 04:00:00",
+        countDown = new Date(donda).getTime(),
+        x = setInterval(function() {
+
+            let now = new Date().getTime(),
+                distance = countDown - now;
+
+            document.getElementById("days").innerText = Math.floor(distance / (day)),
+                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+            if (distance < 0) {
+                let headline = document.getElementById("headline"),
+                    countdown = document.getElementById("countdown"),
+                    content = document.getElementById("content");
+
+                headline.innerText = "Happy YZY SZN!";
+                countdown.style.display = "none";
+                content.style.display = "block";
+
+                clearInterval(x);
+            }
+            //seconds
+        }, 0)
+}());
