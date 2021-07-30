d3d71f9fb8f4a450a839bf0bbbf5c2b9ec0c2f77
Mikołaj Olszyński <insanowskyy@gmail.com>
2021-07-30 21:11:43 +0200

    learn more button

---
+++ stats.js
@@ -0,0 +1,8 @@
+/*<script src="jquery-3.6.0.min.js"></script>
+<script type="text/javascript">
+  let apiKey = 'f5d153dc3269484fb629132f851c6415';
+$.getJSON('https://ipgeolocation.abstractapi.com/v1/?api_key=' + apiKey, function(data) {
+  console.log(JSON.stringify(data, null, 2));
+});</script>
+</body>
+for later :p*/
