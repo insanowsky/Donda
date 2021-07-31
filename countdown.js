2037fc45e061718458d49fcb2bae6a8d0ce942ae
Mikołaj Olszyński <insanowskyy@gmail.com>
2021-07-31 16:20:59 +0200

    new date

--- countdown.js
+++ countdown.js
@@ -4,7 +4,7 @@
         hour = minute * 60,
         day = hour * 24;
 
-    let donda = "2021-08-06T00:00:00Z",
+    let donda = "2021-08-06T02:00:00Z",
         countDown = new Date(donda).getTime(),
         x = setInterval(function() {
 
